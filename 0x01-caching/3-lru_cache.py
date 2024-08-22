00965c4a0d13e918efaf5cfc9b9d0a4383cf7b4f
shaimaa <shaimafikry@yahoo.com>
2024-08-22 19:18:47 +0300

    add 3

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""caching system"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """ caching system"""
+    def __init__(self):
+        """ initation"""
+        super().__init__()
+
+    def put(self, key, item):
+        """adding items fifo"""
+        if key is not None and item is not None:
+            self.cache_data.update({key: item})
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            key_d = next(iter(self.cache_data))
+            del self.cache_data[key_d]
+            print(f"DISCARD: {key_d}")
+
+    def get(self, key):
+        """ print item"""
+        if key is not None:
+            val = self.cache_data.pop(key, None)
+            if val is not None:
+                self.cache_data[key] = val
+        return self.cache_data.get(key)
