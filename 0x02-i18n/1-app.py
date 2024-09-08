6c12abc880fe141e270ecf2500d2b807aeecdcf1
shaimaa <shaimafikry@yahoo.com>
2024-09-08 23:37:13 +0300

    add files 1

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
-""" module of flask"""
+"""module of flask
+"""
 from flask import Flask, render_template
 from flask_babel import Babel
 
 
 app = Flask(__name__)
+
 
 # Use Config to set Babel’s default locale ("en") and timezone ("UTC").
 class Config:
@@ -18,9 +20,12 @@ class Config:
 app.config.from_object(Config)
 babel = Babel(app)
 
+
 @app.route('/')
 def home():
+    """home route"""
     return render_template('1-index.html')
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
