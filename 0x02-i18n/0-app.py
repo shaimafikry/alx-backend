e645bc44c457ee47cca2d733f5095ae19c234927
shaimaa <shaimafikry@yahoo.com>
2024-09-08 23:34:54 +0300

    add2

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
-""" module flask"""
+"""module flask"""
 from flask import Flask, render_template
+
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
+    """ home rout"""
     return render_template('0-index.html')
 
 if __name__ == '__main__':
