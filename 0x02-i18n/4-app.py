1b5f68e04c2503f7cd7bdca57d6d7b6c78d48245
shaimaa <shaimafikry@yahoo.com>
2024-09-09 00:08:43 +0300

    add files 3

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+"""module of flask
+"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+
+
+# Use Config to set Babel’s default locale ("en") and timezone ("UTC").
+class Config:
+    """CONFIG CLASS """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+# to add config from an object
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+SUPPORTED_LOCALES = ['en', 'fr']
+
+
+@babel.localeselector
+def get_locale():
+    """ call parameteres"""
+    locale = request.args.get('locale')
+    if locale in SUPPORTED_LOCALES:
+        return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def home():
+    """ home route"""
+    return render_template('4-index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
